7c5bbc730076158ffaf01f3b21062cfe390d468b
Zachary DuBois <zacharydubois@icloud.com>
2015-10-28 10:06:25 -0400

    Add some base stuff.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: zacharydubois
+ * Date: 2015-10-28 09:58
+ * Project: Koi
+ * License: MIT, Zachary DuBois 2015 (See LICENSE.md)
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+
