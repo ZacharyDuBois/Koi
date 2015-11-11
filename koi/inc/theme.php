8bceea0e4be76f13fe89ce72e7cd9b2a8b1d16ef
Zachary DuBois <zacharydubois@icloud.com>
2015-11-11 16:58:13 -0500

    Add theme class.

---
+++ koi/inc/theme.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * User: zacharydubois
+ * Date: 2015-11-11 16:21
+ * Project: Koi
+ * License: MIT, Zachary DuBois 2015 (See LICENSE.md)
+ */
+
+namespace Koi;
+
+
+class theme {
+    public function listThemes() {
+        $itemList = scandir(KOITPL, array('..', '.'));
+        $themes = array();
+        foreach ($itemList as $item) {
+            if (is_dir(KOITPL . '/' . $item)) {
+
+                $themes[] = $this->getThemeInfo($item);
+            }
+        }
+        return $themes;
+    }
+
+    public function getThemeInfo($theme) {
+        if (!isset($theme) || !is_string($theme)) {
+            throw new koiException("getThemeInfo() did not receive valid theme.");
+        }
+
+        if (!$this->isTheme($theme)) {
+            throw new koiException($theme . "is not a theme.");
+        }
+
+        $data = new dataStore(KOITPL . '/' . $theme . 'theme.json');
+
+        return $data;
+    }
+
+    public function isTheme($theme) {
+        if (!isset($theme) || !is_string($theme)) {
+            throw new koiException('isTheme() did not receive valid theme.');
+        }
+
+        return file_exists(KOITPL . '/' . $theme);
+    }
+}
